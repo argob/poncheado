7b4b0331bbe9c9e46001b20dc409df19a2ba0b93
Nicolás Rougier <nrougier@modernizacion.gob.ar>
2016-07-19 11:20:13 -0300

    modificaciones en precios cuidados

--- src/proyectos/argentina/estatico/precios-cuidados/js/precios-cuidados.js
+++ src/proyectos/argentina/estatico/precios-cuidados/js/precios-cuidados.js
@@ -3,29 +3,29 @@ $(document).ready(function () {
     var showData = $('#show-data');
 
     $.getJSON('js/productos.json', function (data) {
-      console.log(data);
+        console.log(data);
 
         var productos = data.productos.map(function (producto) {
 
-        var html = '';
-        html += '<tr>';
-        html += ' <th scope="row">&nbsp;&nbsp;';
-        html += '   <a data-toggle="collapse" href="#'+ producto.id + '">';
-        html += '     <i class="fa fa-plus text-success" aria-hidden="true"></i>';
-        html += '   </a>';
-        html += ' </th>';
-        html += ' <td>';
-        html += '   <p class="nombre">'+ producto.name + '</p>';
-        html += '   <div class="collapse" id="'+ producto.id + '">';
-        html += '     <p><strong>Categor&iacute;a: </strong><span class="categoria">'+ producto.categoria + '</span></p>';
-        html += '     <p><strong>EAN: </strong><span class="ean">'+ producto.ean + '</span></p>';
-        html += '   </div>';
-        html += ' </td>';
-        html += ' <td class="marca">'+ producto.marca + '</td>';
-        html += ' <td class="medida td-strong">'+ producto.cantidad + '</td>';
-        html += ' <td class="precio td-strong text-success">'+ producto.precioAMBA + '</td>';
-        html += '</tr>';
-        return html;
+            var html = '';
+            html += '<tr>';
+            html += ' <th scope="row">&nbsp;&nbsp;';
+            html += '   <a data-toggle="collapse" href="#'+ producto.id + '">';
+            html += '     <i class="fa fa-plus text-success" aria-hidden="true"></i>';
+            html += '   </a>';
+            html += ' </th>';
+            html += ' <td>';
+            html += '   <p class="nombre">'+ producto.name + '</p>';
+            html += '   <div class="collapse" id="'+ producto.id + '">';
+            html += '     <p><strong>Categor&iacute;a: </strong><span class="categoria">'+ producto.categoria + '</span></p>';
+            html += '     <p><strong>EAN: </strong><span class="ean">'+ producto.ean + '</span></p>';
+            html += '   </div>';
+            html += ' </td>';
+            html += ' <td class="marca">'+ producto.marca + '</td>';
+            html += ' <td class="medida td-strong">'+ producto.cantidad + '</td>';
+            html += ' <td class="precio td-strong text-success">'+ producto.precioAMBA + '</td>';
+            html += '</tr>';
+            return html;
 
         });
 
@@ -35,6 +35,15 @@ $(document).ready(function () {
           var content = productos.join();
           showData.append(content);
         }
+
+        var listOptions = {valueNames: [ 'nombre', 'marca', 'medida', 'precio', 'categoria', 'ean' ] };
+        var productsList = new List('productos', listOptions);
+    });
+
+    //cambiando clase de icons en tablas
+    $('body').on('click', '.list a', function(){
+        $(this).find('i').toggleClass('fa-plus fa-minus'); 
+        $(this).find('i').toggleClass('text-success text-danger'); 
     });
 
 });
