e4827bd19c58cf04d32340ab74fff1c5b3abffeb
darduino <diego.arduino@gmail.com.ar>
2016-07-20 12:43:17 -0300

    precios cuidados - leyendo productos json

--- src/proyectos/argentina/estatico/precios-cuidados/js/precios-cuidados.js
+++ src/proyectos/argentina/estatico/precios-cuidados/js/precios-cuidados.js
@@ -2,39 +2,37 @@ $(document).ready(function () {
 
     var showData = $('#show-data');
 
-    $.getJSON('js/productos.json', function (data) {
-        console.log(data);
+    $.getJSON('https://spreadsheets.google.com/feeds/list/1EreI6pWCVbiqbyURniaRx-keRHzBhyZ-6p01VSqOtms/1/public/values?alt=json', function (data) {
+        //console.log(data);
 
-        var productos = data.productos.map(function (producto) {
+        var html = '';
+        $.each( data.feed.entry, function( key, item ) {
+            //console.log("testeo"+item.gsx$ean.$t);
 
-            var html = '';
+            
             html += '<tr>';
             html += ' <th scope="row">&nbsp;&nbsp;';
-            html += '   <a data-toggle="collapse" href="#'+ producto.id + '">';
+            html += '   <a data-toggle="collapse" href="#id_'+ key + '">';
             html += '     <i class="fa fa-plus text-success" aria-hidden="true"></i>';
             html += '   </a>';
             html += ' </th>';
             html += ' <td>';
-            html += '   <div class="nombre">'+ producto.name + '</div>';
-            html += '   <div class="collapse" id="'+ producto.id + '">';
-            html += '     <div><strong>Categor&iacute;a: </strong><span class="categoria">'+ producto.categoria + '</span></div>';
-            html += '     <div><strong>EAN: </strong><span class="ean">'+ producto.ean + '</span></div>';
+            html += '   <div class="nombre">'+ item.gsx$producto.$t + '</div>';
+            html += '   <div class="collapse" id="id_'+ key + '">';
+            html += '     <div><strong>Categor&iacute;a: </strong><span class="categoria">'+ item.gsx$categoría.$t + '</span></div>';
+            html += '     <div><strong>EAN: </strong><span class="ean">'+ item.gsx$ean.$t + '</span></div>';
             html += '   </div>';
             html += ' </td>';
-            html += ' <td class="marca">'+ producto.marca + '</td>';
-            html += ' <td class="medida td-strong">'+ producto.cantidad + '</td>';
-            html += ' <td class="precio td-strong text-success">'+ producto.precioAMBA + '</td>';
+            html += ' <td class="marca">'+ item.gsx$marca.$t + '</td>';
+            html += ' <td class="medida td-strong">'+ item.gsx$cantidad.$t + '</td>';
+            html += ' <td class="precio td-strong text-success">'+ item.gsx$amba.$t + '</td>';
             html += '</tr>';
-            return html;
-
+    
         });
 
         showData.empty();
+        showData.append(html);
 
-        if (productos.length) {
-          var content = productos.join();
-          showData.append(content);
-        }
 
         var listOptions = {valueNames: [ 'nombre', 'marca', 'medida', 'precio', 'categoria', 'ean' ] };
         var productsList = new List('productos', listOptions);
