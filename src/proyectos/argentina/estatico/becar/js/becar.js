5b926ff99fc50f6b9047b83e615f8fffa46b477f
darduino <diego.arduino@gmail.com.ar>
2016-08-12 17:09:11 -0300

    pagina becar, funcionalidad filtros

--- src/proyectos/argentina/estatico/becar/js/becar.js
+++ src/proyectos/argentina/estatico/becar/js/becar.js
@@ -3,17 +3,23 @@ $(document).ready(function () {
 
 
     $.getJSON('https://spreadsheets.google.com/feeds/list/1CFdttPTD_zPz6awvGZ_UfiDB7wJDALtSsoJTY62nIlM/1/public/values?alt=json', function (data) {
-        window.products = data.feed.entry;
-        showProducts('todas');
+        window.convocatorias = data.feed.entry;
+        showConvocatorias('todas');
         console.log(data);
+
     });
 
-    window.showProducts = function(modalidad){
+    window.showConvocatorias = function(modalidad){
         var showData = $('#show-data');
-
+        console.log("entro"); 
+        console.log(modalidad); 
         var html = '';
-        $.each( products, function( key, item ) {
-            console.log("testeo"+item.gsx$nombre.$t);
+        $.each( convocatorias, function( key, item ) {
+
+            if(modalidad == 'todas' || modalidad == item.gsx$modalidad.$t){
+                console.log(modalidad); 
+            console.log(item.gsx$modalidad.$t);
+                //console.log("testeo"+item.gsx$nombre.$t);
             html += '<tr data-toggle="collapse" href="#id_'+ key + '">';
             html += ' <th scope="row">&nbsp;&nbsp;';
             html += '   <a data-toggle="collapse" href="#id_'+ key + '">';
@@ -23,24 +29,26 @@ $(document).ready(function () {
             html += ' <td>';
             html += '   <div class="nombre">'+ item.gsx$nombre.$t + '</div>';
             html += '   <div class="collapse" id="id_'+ key + '">';
-            html += '     <div><strong>Fecha de inicio: </strong><span class="categoria">'+ item.gsx$fechainicio.$t + '</span></div>';
-            html += '     <div><strong>Fecha de fin: </strong><span class="ean">'+ item.gsx$fechafin.$t + '</span></div>';
+            html += '     <div><strong>Fecha de inicio: </strong><span class="fechainicio">'+ item.gsx$fechainicio.$t + '</span></div>';
+            html += '     <div><strong>Fecha de fin: </strong><span class="fechafin">'+ item.gsx$fechafin.$t + '</span></div>';
             html += '   </div>';
             html += ' </td>';
-            html += ' <td class="marca">'+ item.gsx$instituciones.$t + '</td>';
-            html += ' <td class="medida">'+ item.gsx$áreatemática.$t + '</td>';
-            html += ' <td class="medida">'+ item.gsx$paises.$t + '</td>';
-            html += ' <td class="medida">'+ item.gsx$modalidad.$t + '</td>';
+            html += ' <td class="instituciones">'+ item.gsx$instituciones.$t + '</td>';
+            html += ' <td class="areaTematica">'+ item.gsx$áreatemática.$t + '</td>';
+            html += ' <td class="paises">'+ item.gsx$paises.$t + '</td>';
+            html += ' <td class="modalidad">'+ item.gsx$modalidad.$t + '</td>';
             //html += ' <td class="precio td-strong text-success">$'+ item['gsx$'+modalidad].$t + '</td>';
-            html += '</tr>';
+            html += '</tr>';    
+            }
+            
     
         });
 
         showData.empty();
         showData.append(html);
 
-        var listOptions = {valueNames: [ 'nombre', 'marca', 'medida', 'precio', 'categoria', 'ean' ] };
-        var productsList = new List('productos', listOptions);
+        var listOptions = {valueNames: [ 'nombre', 'instituciones', 'areaTematica', 'paises', 'modalidad' ] };
+        var convocatoriasList = new List('convocatorias', listOptions);
     }
 
     //cambiando clase de icons en tablas
@@ -52,7 +60,7 @@ $(document).ready(function () {
     $(".btn-group a").on("click", function() {
       $(this).addClass('active').siblings().removeClass('active'); 
       var modalidad = $(this).data("modalidad");
-      showProducts(modalidad);
+      showConvocatorias(modalidad);
     });    
 
 });
