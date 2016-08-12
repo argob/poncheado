9c24863bed75c40ad4defb80c29c5600d2fbbba7
darduino <diego.arduino@gmail.com.ar>
2016-08-12 12:57:08 -0300

    banco de evaluaciones, agregando busqueda por evaluacion

--- src/proyectos/argentina/estatico/banco-evaluaciones/js/banco-evaluaciones.js
+++ src/proyectos/argentina/estatico/banco-evaluaciones/js/banco-evaluaciones.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         $.each( evaluaciones, function( key, item ) {
             //console.log("testeo"+item.gsx$ean.$t);
             html += '<tr>';
-            html += ' <td class="titulo"> <h6>'+ item.gsx$titulo.$t + '</h6><p class="margin-0">'+ item.gsx$programa.$t + '</p><p><small>' + 'Evaluación: ' + item.gsx$evaluacion.$t + '</small></p><p class="margin-0"><a target="_blank" href="'+ item.gsx$fichatecnica.$t + '" class="btn btn-primary btn-sm"><i class="fa fa-download"></i> Ficha Técnica </a>&nbsp;&nbsp<a target="_blank" href="'+ item.gsx$evaluacion_2.$t + '" class="btn btn-primary btn-sm"><i class="fa fa-download"></i> Evaluación</a></p></td>';
+            html += ' <td class="titulo"> <h6 class="evaluacion">'+ item.gsx$titulo.$t + '</h6><p class="margin-0">'+ item.gsx$programa.$t + '</p><p><small>' + 'Evaluación: ' + item.gsx$evaluacion.$t + '</small></p><p class="margin-0"><a target="_blank" href="'+ item.gsx$fichatecnica.$t + '" class="btn btn-primary btn-sm"><i class="fa fa-download"></i> Ficha Técnica </a>&nbsp;&nbsp<a target="_blank" href="'+ item.gsx$evaluacion_2.$t + '" class="btn btn-primary btn-sm"><i class="fa fa-download"></i> Evaluación</a></p></td>';
             html += ' <td class="organismo">'+ item.gsx$organismo.$t + '</td>';
             html += ' <td class="tema">'+ item.gsx$tema.$t + '</td>';
             html += ' <td class="ano">'+ item.gsx$ano.$t + '</td>';
@@ -26,7 +26,7 @@ $(document).ready(function () {
         showData.empty();
         showData.append(html);
 
-        var listOptions = {valueNames: [ 'organismo', 'tema', 'ano' ] };
+        var listOptions = {valueNames: [ 'evaluacion', 'organismo', 'tema', 'ano' ] };
         var evaluacionesList = new List('evaluaciones', listOptions);
     }
 
