1193f9d6b579d9b8a0ed2ec5756260a2c88b76f8
Nicolás Rougier <nrougier@modernizacion.gob.ar>
2016-09-06 16:35:00 -0300

    Modificaciones en País Digital y agrega proyecto de presupuesto

---
+++ src/proyectos/presupuesto/js/charts.js
@@ -0,0 +1,150 @@
+google.load('visualization', '1.0', {'packages':['corechart']});
+
+var data = {};
+var chart = {};
+var options = {
+  area: {
+    curveType: 'function',
+    backgroundColor: '#f9f9f9',
+    colors: [
+      '#006aa9',
+      '#ca1767',
+      '#e55535',
+      '#ef9116',
+      '#51a42b',
+      // '#009c9a',
+      // '#00a474',
+      // '#fbc900',
+      // '#e3494d',
+      // '#df3671',
+    ],
+    lineWidth: 2,
+    areaOpacity: 0.2,
+    fontName: 'Roboto',
+    fontSize: 14,
+    isStacked: true,
+    chartArea: {
+      left:'10%',
+      top: '10%',
+      width:'80%',
+      height:'65%'
+    },
+    hAxis: {
+      gridlines: {
+        color: '#f9f9f9',
+      }
+    },
+    vAxis: {
+      baselineColor: '#AAA',
+      format: 'short',
+      minValue: 0,
+      gridlines: {
+        color: '#E8E8E8'
+      }
+    },
+    legend: {
+      position: 'bottom'
+    }
+  },
+  bar: {
+    backgroundColor: '#f9f9f9',
+    colors: [
+      '#006aa9',
+      '#ca1767',
+      '#e55535',
+      '#ef9116',
+      '#51a42b',
+    ],
+    fontName: 'Roboto',
+    fontSize: 14,
+    chartArea: {
+      left:'10%',
+      top: '10%',
+      width:'85%',
+      height:'65%'
+    },
+    legend: {
+      position: 'bottom'
+    },
+    hAxis: {
+      gridlines: {
+        color: '#f9f9f9',
+      }
+    },
+    vAxis: {
+      baselineColor: '#AAA',
+      format: 'short',
+      minValue: 0,
+      gridlines: {
+        color: '#E8E8E8'
+      }
+    }
+  },
+  line: {
+    backgroundColor: '#f9f9f9',
+    colors: [
+      '#006aa9',
+      '#ca1767',
+      '#e55535',
+      '#ef9116',
+      '#51a42b',
+    ],
+    lineWidth: 2,
+    fontName: 'Roboto',
+    fontSize: 14,
+    chartArea: {
+      left:'10%',
+      top: '10%',
+      width:'80%',
+      height:'65%'
+    },
+    legend: {
+      position: 'bottom'
+    },
+    hAxis: {
+      gridlines: {
+        color: '#f9f9f9',
+      }
+    },
+    vAxis: {
+      baselineColor: '#AAA',
+      format: 'short',
+      minValue: 0,
+      gridlines: {
+        color: '#E8E8E8'
+      }
+    },
+    interpolateNulls: true
+  },
+  pie: {
+    backgroundColor: '#f9f9f9',
+    colors: [
+      '#006aa9',
+      '#007EC7',
+      '#008EE1',
+      '#009EFA',
+      '#14A9FF',
+      '#2EB3FF',
+      '#47BCFF',
+      '#61C5FF'
+    ],
+    fontName: 'Roboto',
+    fontSize: 14,
+    chartArea: {
+      left:'10%',
+      top: '10%',
+      width:'85%',
+      height:'65%'
+    },
+    pieSliceTextStyle: {
+      fontSize: 12
+    }
+  }
+};
+
+
+
+
+
+
+
