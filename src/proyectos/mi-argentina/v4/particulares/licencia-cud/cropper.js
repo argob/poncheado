88241cfe2f5abf700314630d62f78a6a1f99e973
MGuadalupe <gvargas@modernizacion.gob.ar>
2019-04-24 15:45:04 -0300

    fix varios

--- src/proyectos/mi-argentina/v4/particulares/licencia-cud/cropper.js
+++ src/proyectos/mi-argentina/v4/particulares/licencia-cud/cropper.js
@@ -96,7 +96,7 @@
     restore: true,
 
     // Check if the current image is a cross-origin image
-    checkCrossOrigin: true,
+    checkCrossOrigin: false,
 
     // Check the current image's Exif Orientation information
     checkOrientation: true,
