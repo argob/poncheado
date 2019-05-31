f4f1d5b26baa497a39d420ae2ec3f3a284f6ff73
MGuadalupe <gvargas@modernizacion.gob.ar>
2019-05-31 17:36:11 -0300

    nueva credencial MA

--- src/proyectos/mi-argentina/v4/js/alert-collapse.js
+++ src/proyectos/mi-argentina/v4/js/alert-collapse.js
@@ -3,12 +3,12 @@
 		
 
 		$('.collapse').on('hidden.bs.collapse', function () {
-			console.log($(this));
+			
 			$(this).parents('.panel-default').find('.fa-chevron-up').addClass('collapseGira');
 		})
 
 
 		$('.collapse').on('show.bs.collapse', function () {
-			console.log($(this));
+		
 			$(this).parents('.panel-default').find('.fa-chevron-up').removeClass('collapseGira');
 		})
