174d58dce674d81fd01893b6854ec4cba81a9547
pablomaurig <pablomaurig@gmail.com>
2019-06-13 16:57:33 -0300

    Cambios en steps. elimino pasos a la izquierda en mobile

--- src/proyectos/mi-argentina/v4/js/steps.js
+++ src/proyectos/mi-argentina/v4/js/steps.js
@@ -1,53 +1,30 @@
 $(document).ready(function () {
-	//Initialize tooltips
+	//Inicia tooltips
 	$('.nav-tabs > li a[title]').tooltip();
-	
-	//Wizard
+	//pasos wizard
 	$('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-
 		var target = $(e.target);
-	
-		if (target.parent().hasClass('disabled')) {
+			if (target.parent().hasClass('disabled')) {
 			return false;
 		}
 	});
-
+	// click en boton siguiente paso
 	$(".next-step").click(function (e) {
-
 		var active = $('.wizard-head .nav-tabs li.active');
-		var miact = $('.tab-content .mobile-step.activo');
 		active.next().removeClass('disabled');
-		miact.removeClass('activo');
-		miact.next().next().addClass('activo').removeClass('disabled');
-		
 		nextTab(active);
-
 	});
+	// click en boton anterior
 	$(".prev-step").click(function (e) {
-
-		var active = $('.wizard-head .nav-tabs li.active');
-		var miact = $('.tab-content .mobile-step.activo');
-
-		
-		miact.removeClass('activo');
-		miact.prev().prev().addClass('activo');
-		miact.next().next().removeClass('activo');
-
+		var active = $('.wizard-head .nav-tabs li.active');		
 		prevTab(active);
-
-	});
-	$(".mobile-step > a").click(function(e){
-		$(".mobile-step").removeClass('activo');
-		$(this).parent().addClass('activo');
-		e.preventDefault();
-		var link = $(this).attr('href');
-		$(".nav-tabs a[href='"+link+"']").trigger("click");
 	});
 });
-
+// Simula clic en siguiente tab
 function nextTab(elem) {
 	$(elem).next().find('a[data-toggle="tab"]').click();
 }
+// Simula clic en el tab anterior
 function prevTab(elem) {
 	$(elem).prev().find('a[data-toggle="tab"]').click();
 }
