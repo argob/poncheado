68b23f8ad5db53cc57dd4d2900a89323ebd24dc3
Juan Manuel Docile <jmdocile@gmail.com>
2022-12-14 23:29:07 -0300

    Eliminamos el resto

--- src/proyectos/mi-argentina/js/alert-close.js
+++
@@ -1,7 +0,0 @@
-
-		var alert = $('.pclose .btn-close');
-		alert.on('click', function(){
-			$(this).parents('.pclose').fadeOut(200);
-		})
-
-		
--- src/proyectos/mi-argentina/js/onboarding.js
+++
@@ -1,9 +0,0 @@
-var modal = document.getElementById('myModal')
-
-var closeModal = document.getElementById('close')
-console.log(closeModal)
-	window.onload = function(){
-		modal.style.display = 'block';
-	}
-
-	
